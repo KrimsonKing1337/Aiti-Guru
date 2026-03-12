69effcb4c99dbe139b59c4c242003a606e338202
Oleg Shilov <shilov-1@yandex.ru>
2026-03-12 16:44:25 +0300

    always go though main page first;

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,8 +1,18 @@
+import { useEffect } from 'react';
+
+import { useNavigate } from 'react-router';
+
 import { ToastContainer } from 'react-toastify';
 
 import 'styles/styles.scss';
 
 export const App = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    navigate('/');
+  }, []);
+
   return (
     <ToastContainer
       hideProgressBar
