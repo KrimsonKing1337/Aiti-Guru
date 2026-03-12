309ab691582fa413cc0d45475afdd6ff984c46b9
Oleg Shilov <shilov-1@yandex.ru>
2026-03-12 13:16:03 +0300

    checking login as password before fetching;

--- src/components/Auth/components/LoginButton/LoginButton.tsx
+++ src/components/Auth/components/LoginButton/LoginButton.tsx
@@ -2,6 +2,8 @@ import { useEffect } from 'react';
 
 import { useNavigate } from 'react-router';
 
+import { toast } from 'react-toastify';
+
 import { useDispatch, useSelector } from 'react-redux';
 
 import { loginActions, loginSelectors } from 'store/login';
@@ -12,6 +14,8 @@ export const LoginButton = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
+  const login = useSelector(loginSelectors.login);
+  const password = useSelector(loginSelectors.password);
   const isFetchSuccess = useSelector(loginSelectors.isFetchSuccess);
 
   useEffect(() => {
@@ -21,6 +25,12 @@ export const LoginButton = () => {
   }, [isFetchSuccess]);
 
   const clickHandler = () => {
+    if (!login || !password) {
+      toast.error('Поля логин и пароль обязательны для заполнения');
+
+      return;
+    }
+
     dispatch(loginActions.fetch());
   };
 
