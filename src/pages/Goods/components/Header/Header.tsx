0867312eb29d76c0b286c26281a7dac6472b7603
Oleg Shilov <shilov-1@yandex.ru>
2026-03-13 16:07:19 +0300

    added debounce;

--- src/pages/Goods/components/Header/Header.tsx
+++ src/pages/Goods/components/Header/Header.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from 'react';
+
 import { useDispatch, useSelector } from 'react-redux';
 
 import { goodsActions, goodsSelectors } from 'store/goods';
@@ -9,17 +11,35 @@ export const Header = () => {
 
   const search = useSelector(goodsSelectors.search);
 
+  const [value, setValue] = useState(search);
+
   const inputChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
-    dispatch(goodsActions.setSearch(e.target.value));
+    setValue(e.target.value);
   };
+
+  useEffect(() => {
+    if (value.length === 1) {
+      return;
+    }
+
+    const timer = setTimeout(() => {
+      dispatch(goodsActions.setSearch(value));
+    }, 500);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [value, dispatch]);
 
   return (
     <div className={styles.Wrapper}>
-      <div>
-        Товары
-      </div>
+      <div>Товары</div>
 
-      <input placeholder="Найти" value={search} onChange={inputChangeHandler} />
+      <input
+        placeholder="Найти"
+        value={value}
+        onChange={inputChangeHandler}
+      />
     </div>
   );
 };
