98507a06518a534881c9efdbffd7d9b689567681
Oleg Shilov <shilov-1@yandex.ru>
2026-03-13 09:35:23 +0300

    renamed login slice to auth;

--- src/store/auth/selectors.ts
+++ src/store/auth/selectors.ts
@@ -1,10 +1,10 @@
 import type { RootState } from '../store';
 
 export const selectors = {
-  login: (state: RootState) => state.login.login,
-  password: (state: RootState) => state.login.password,
-  isAuthed: (state: RootState) => state.login.isAuthed,
-  isFetching: (state: RootState) => state.login.isFetching,
-  isFetchSuccess: (state: RootState) => state.login.isFetchSuccess,
-  fetchError: (state: RootState) => state.login.fetchError,
+  login: (state: RootState) => state.auth.login,
+  password: (state: RootState) => state.auth.password,
+  isAuthed: (state: RootState) => state.auth.isAuthed,
+  isFetching: (state: RootState) => state.auth.isFetching,
+  isFetchSuccess: (state: RootState) => state.auth.isFetchSuccess,
+  fetchError: (state: RootState) => state.auth.fetchError,
 };
