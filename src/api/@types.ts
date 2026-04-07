dd8d66a570a3dea835840090e2c2a323696e3008
Oleg Shilov <shilov-1@yandex.ru>
2026-04-07 11:24:23 +0300

    fixes after review;

--- src/api/@types.ts
+++ src/api/@types.ts
@@ -43,7 +43,7 @@ export type FetchProductsParams = {
   limit: number;
   skip: number;
   sortBy?: ProductSortField;
-  order: ProductSortOrder;
+  order?: ProductSortOrder;
   search?: string;
 };
 
